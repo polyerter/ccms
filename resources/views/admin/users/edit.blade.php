73fae90f9f5ea76565863697ad4b03cf1c6a1d38
kastembay <kastet_forever@mail.ru>
2019-01-29 16:46:22 +0600

    Change auth e-mail to login

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -8,9 +8,6 @@
   <link href="{{ asset('css/admin/users/css/create_user.css') }}" rel="stylesheet" type="text/css">
   <link href="{{ asset('css/admin/settings.css') }}" rel="stylesheet" type="text/css">
 
-{{--  <link href="{{ asset('css/admin/users/css/jstree.min.css') }}" rel="stylesheet" type="text/css">--}}
-  {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/themes/default/style.min.css"/>--}}
-
 
 @endpush
 
@@ -25,9 +22,6 @@
   <script src="{{ asset('js/admin/users/js/datepicker.en.js') }}"></script>
   <script src="{{ asset('js/admin/init/form-datepicker-init.js') }}"></script>
 
-
-{{--  <script src="{{ asset('js/admin/users/js/jstree.min.js') }}"></script>--}}
-{{--  <script src="{{ asset('js/admin/init/jstree-init.js') }}"></script>--}}
   {{--генератор пароля--}}
   <script type="text/javascript">
       function urlLit(w,v) {
@@ -64,79 +58,6 @@
           $('#pass').val(pass);
       }
   </script>
-
-  {{--<script>--}}
-      {{--$.ajaxSetup({--}}
-          {{--beforeSend: function(xhr, type) {--}}
-              {{--if (!type.crossDomain) {--}}
-                  {{--xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));--}}
-              {{--}--}}
-          {{--},--}}
-      {{--});--}}
-      {{--function updatePermissions(id, li) {--}}
-          {{--var items = [];--}}
-          {{--/* Формирует массив прав к отправке */--}}
-          {{--for (var i = 0; i < li.length; i++) {--}}
-              {{--items.push(parseInt(li[i].id));--}}
-          {{--}--}}
-          {{--if (items.length) {--}}
-              {{--$.ajax({--}}
-                  {{--type: 'post',--}}
-                  {{--url: '{{ url('admin/permissions/set') }}',--}}
-                  {{--data: {--}}
-                      {{--user: id,--}}
-                      {{--data: items,--}}
-                      {{--_token: '{!! csrf_token() !!}',--}}
-                  {{--},--}}
-                  {{--success: function (data) {--}}
-                  {{--}--}}
-              {{--});--}}
-          {{--}--}}
-      {{--}--}}
-      {{--$(document).ready(function () {--}}
-          {{--/**--}}
-           {{--//  todo Получение прав пользователя, проходит по всем элементам с классом permissions--}}
-           {{--**/--}}
-
-          {{--$('.permissions').each(function (ind, el) {--}}
-              {{--var id = $(el).data('id'); // получение id пользователя--}}
-              {{--$.post('{{ url('admin/permissions/get') }}', {user: id}, function (data) { // запрос на получение прав--}}
-                  {{--/* Построение дерева прав */--}}
-                  {{--// console.log({data}); todo вывод в консоль запроса массива--}}
-                  {{--$('.permissions').jstree({--}}
-                      {{--'plugins': [--}}
-                          {{--"wholerow",--}}
-                          {{--"checkbox"--}}
-                      {{--],--}}
-                      {{--"checkbox": {--}}
-                          {{--"real_checkboxes": false,--}}
-                          {{--"three_state": true--}}
-                      {{--},--}}
-                      {{--'core': {--}}
-                          {{--"themes": {--}}
-                              {{--"icons": false--}}
-                          {{--},--}}
-                          {{--'data': data--}}
-                      {{--}--}}
-                  {{--})--}}
-
-                      {{--.on('deselect_node.jstree', function (e, data) {--}}
-                          {{--updatePermissions(id, data.instance.get_selected(true));--}}
-                      {{--})--}}
-                      {{--.on('select_node.jstree', function (e, data) {--}}
-                          {{--updatePermissions(id, data.instance.get_selected(true));--}}
-                      {{--})--}}
-                      {{--.on('ready.jstree', function () {--}}
-                          {{--$(this).jstree("close_all");--}}
-                      {{--});--}}
-
-              {{--});--}}
-          {{--});--}}
-
-      {{--})--}}
-  {{--</script>--}}
-
-  {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/jstree.min.js"></script>--}}
 
 @endpush
 
